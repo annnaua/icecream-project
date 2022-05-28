0dd261b802004a7dae056c9cb33094adb6ef1d94
annnaua <96844121+annnaua@users.noreply.github.com>
2022-05-28 22:52:17 +0000

    Deploying to gh-pages from @ annnaua/icecream-project@6567d0772b69fd40897d048830aad49458c47bd5 🚀

--- index.9a42c839.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.9a42c839.js.map
---
+++ index.f25ea74b.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0},grabCursor:!0,touchRatio:1,simulateTouch:!0});
+//# sourceMappingURL=index.f25ea74b.js.map
