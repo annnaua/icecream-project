f2abcc4feada0e04bec9fc7a5605a4442b50c4b8
Anna Riabova <96844121+annnaua@users.noreply.github.com>
2022-05-29 01:53:56 +0300

    Revert "reviews2"

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,8 +2,5 @@ var swiper = new Swiper(".mySwiper", {
                 pagination: {
                     el: ".swiper-pagination",
                     clickable: true,
-    },
-    grabCursor: true,
-    touchRatio: 1,
-    simulateTouch: true,
+                },
             });
