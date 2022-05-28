5e0e3a4d617b6c0ed91bce6aab5ffaab37fae52b
Arina Grushchenko <arinagrushchenko@gmail.com>
2022-05-28 23:38:25 +0300

    grabCursor

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,5 +2,8 @@ var swiper = new Swiper(".mySwiper", {
                 pagination: {
                     el: ".swiper-pagination",
                     clickable: true,
-                },
+    },
+    grabCursor: true,
+    touchRatio: 1,
+    simulateTouch: true,
             });
