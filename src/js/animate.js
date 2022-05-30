30f33cb1d0ef2639d2d70db702fa1f970bb62526
Maksi16 <97921034+Maksi16@users.noreply.github.com>
2022-05-30 23:37:21 +0300

    add animate for advantages

---
+++ src/js/animate.js
@@ -0,0 +1,13 @@
+const square = document.querySelector('.animates');
+square.classList.remove('animates-transition');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('animates-transition');
+      return;
+    }
+    square.classList.remove('animates-transition');
+  });
+});
+observer.observe(document.querySelector('.animates'));
