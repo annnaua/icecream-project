e620356b66cf0464e4c31ffbc1738687ba7f4385
annnaua <96844121+annnaua@users.noreply.github.com>
2022-05-31 09:50:35 +0000

    Deploying to gh-pages from @ annnaua/icecream-project@cb8828d517d370fa901e8b7daa3c25be0a9f44a8 🚀

---
+++ index.2f2701d4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header-burger"),r=document.querySelector(".header-menu"),t=document.querySelector("body");e.addEventListener("click",(function(a){e.classList.toggle("active"),r.classList.toggle("active"),t.classList.toggle("lock")}))}();var pageHeader=document.querySelector(".header");window.addEventListener("scroll",(function(e){window.scrollY>0?pageHeader.classList.add("header-bg"):pageHeader.classList.remove("header-bg")}));
+//# sourceMappingURL=index.2f2701d4.js.map
--- index.c8ea65d4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".header-burger"),t=document.querySelector(".header-menu"),c=document.querySelector("body");e.addEventListener("click",(function(o){e.classList.toggle("active"),t.classList.toggle("active"),c.classList.toggle("lock")}))}();
-//# sourceMappingURL=index.c8ea65d4.js.map
